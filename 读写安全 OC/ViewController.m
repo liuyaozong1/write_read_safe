4a63a1574d3e18bf4bee08d4b6c76d1b93c4ed0b
liuyaozong1 <648731281@qq.com>
2022-01-25 10:27:54 +0800

    增加 gcd 栅栏方式

--- 读写安全 OC/ViewController.m
+++ 读写安全 OC/ViewController.m
@@ -24,12 +24,28 @@
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    [self gcdBarrierAction];
+}
+//读写锁 来实现
+-(void)lockAction {
     for (int i = 0; i < 10; i ++) {
         dispatch_async(self.queue, ^{
             [[[NSThread  alloc] initWithTarget:self selector:@selector(write) object:nil] start];
         });
         dispatch_async(self.queue, ^{
             [[[NSThread  alloc] initWithTarget:self selector:@selector(read) object:nil] start];
+        });
+    }
+}
+
+//GCD栅栏实现
+-(void)gcdBarrierAction {
+    for (int i = 0; i < 10; i ++) {
+        dispatch_async(self.queue, ^{
+            [[[NSThread  alloc] initWithTarget:self selector:@selector(write2) object:nil] start];
+        });
+        dispatch_async(self.queue, ^{
+            [[[NSThread  alloc] initWithTarget:self selector:@selector(read2) object:nil] start];
         });
     }
 }
@@ -48,6 +64,21 @@
     sleep(1);
     NSLog(@"read");
     pthread_rwlock_unlock(&_lock);
+}
+
+//写数据
+-(void)write2 {
+    dispatch_barrier_async(self.queue, ^{
+        sleep(1);
+        NSLog(@"write");
+    });
+}
+//读数据
+-(void)read2 {
+    dispatch_async(self.queue, ^{
+        sleep(1);
+        NSLog(@"read");
+    });
 }
 
 -(void)dealloc {
